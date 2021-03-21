117c157d2fb0c2b173b53cefb1cb5ebf9c1ea5e7
Sefa Diker <sfdiker@gmail.com>
2021-03-21 14:10:35 +0300

    files added.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div >
+      <div style={{ background: '#DADADA', marginBottom: '1.5vmax', padding: '4.6vmax', fontSize: '24px' }}>
+        <a>SeismoCare</a>
+
+      </div>
+      <div>
+        <p style={{ background: '#DADADA', marginBottom: '1.5vmax', padding: '4.6vmax' }}>
+          seismocare@gmail.com</p>
+      </div>
+
+    </div >
+  );
+}
+
+export default App;
