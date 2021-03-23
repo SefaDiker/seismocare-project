f1aad1ebb830ed27c87b95fb00bafa2877444597
Sefa Diker <sfdiker@gmail.com>
2021-03-23 13:45:42 +0300

    updated

--- server.js
+++ server.js
@@ -8,20 +8,10 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
-<<<<<<< HEAD
-    return res.send('pong');
-});
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(port);
-=======
- return res.send('pong');
+  return res.send('pong');
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
->>>>>>> 3bcc9da1f3243be06e29f424c5d354a5b6dd6943
