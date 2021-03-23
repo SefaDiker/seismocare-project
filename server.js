4552a447532a29c61d8d7d99ecca5e38d04bd766
SefaDiker <59263089+SefaDiker@users.noreply.github.com>
2021-03-23 13:40:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,15 +8,6 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
-<<<<<<< HEAD
-    return res.send('pong');
-});
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(port);
-=======
  return res.send('pong');
 });
 app.get('/', function (req, res) {
@@ -24,4 +15,3 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port);
->>>>>>> 3bcc9da1f3243be06e29f424c5d354a5b6dd6943
